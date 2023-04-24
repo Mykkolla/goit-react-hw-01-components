49197ebedc2a40b2bd64199b416864c3581afa8e
Mykkolla <tarabukin.n.10@gmail.com>
2023-04-24 22:36:28 +0300

    stat

--- src/index.js
+++ src/index.js
@@ -3,8 +3,91 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const user = require('./user.json');
+
+// const Profile = ({
+//   username,
+//   tag,
+//   location,
+//   avatar,
+//   stats
+// }) => (
+//   <div className="profile">
+//     <div className="description">
+//       <img
+//         src={avatar}
+//         alt={username}
+//         className="avatar"
+//       />
+//       <p className="name">{username}</p>
+//       <p className="tag">{tag}</p>
+//       <p className="location">{location}</p>
+//     </div>
+
+//     <ul className="stats">
+//       <li>
+//         <span className="label">followers</span>
+//         <span className="quantity">{stats.followers}</span>
+//       </li>
+//       <li>
+//         <span className="label">views</span>
+//         <span className="quantity">{stats.views}</span>
+//       </li>
+//       <li>
+//         <span className="label">likes</span>
+//         <span className="quantity">{stats.likes}</span>
+//       </li>
+//     </ul>
+//   </div>
+// );
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  const { followers, views, likes } = stats;
+
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+// const Product = ({ user }) => (
+//   <>
+//     <Profile {...user} />
+//   </>
+// );
+
+const rootElement = document.getElementById("root");
+ReactDOM.createRoot(rootElement).render(
+<Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+  // <React.StrictMode>
+  //   <Product user={user} />
+  // </React.StrictMode>
 );
+
+
