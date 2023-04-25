c4e72d2ccf7c1918f6d321bb181fdee4b688706a
Mykkolla <tarabukin.n.10@gmail.com>
2023-04-25 22:05:15 +0300

    start2

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,4 +1,4 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 const TransactionHistory = ({ items }) => {
     return (
@@ -25,3 +25,16 @@ const TransactionHistory = ({ items }) => {
   };
 
   export default TransactionHistory;
+
+
+
+  TransactionHistory.propTypes = {
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            type: PropTypes.string.isRequired,
+            amount: PropTypes.string.isRequired,
+            currency: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+};
